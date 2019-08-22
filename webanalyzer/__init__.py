a7c09df884d104647f555f3f7591d02c65eda8c1
fate0 <fate0@fatezero.org>
2019-08-22 23:16:21 +0800

    Bump Version

--- webanalyzer/__init__.py
+++ webanalyzer/__init__.py
@@ -3,7 +3,7 @@
 
 __email__ = 'fate0@fatezero.org'
 __author__ = 'fate0'
-__version__ = '2019.08.17'
+__version__ = '2019.08.22'
 
 from . import condition, webanalyzer
 from .condition import Condition
